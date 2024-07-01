4aa33435351b8864a11eb1eb2bf15adef0728e79
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-01 13:44:14 +0800

    Progress: Update return transactions index page

--- resources/views/dashboard/return-transaction/detail.blade.php
+++ resources/views/dashboard/return-transaction/detail.blade.php
@@ -49,6 +49,34 @@
                                         value="{{ $transaction->booking->vehicle->name . ' - ' . $transaction->booking->vehicle->license_plate_number . ' - ' . $transaction->booking->vehicle->capacity . ' person' }}">
                                 </div>
                             </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="total_price">Total Price</label>
+                                    <input type="text" class="input w-100" name="total_price" id="total_price" readonly
+                                        value="{{ formatRupiah($transaction->total_price) }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="total_paid">Total Paid</label>
+                                    <input type="text" class="input w-100" name="total_paid" id="total_paid" readonly
+                                        value="{{ formatRupiah($transaction->total_paid) }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="total_change">Total Change</label>
+                                    <input type="text" class="input w-100" name="total_change" id="total_change" readonly
+                                        value="{{ formatRupiah($transaction->total_change) }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="payment_method">Payment Method</label>
+                                    <input type="text" class="input w-100 text-capitalize" name="payment_method"
+                                        id="payment_method" readonly value="{{ $transaction->payment_method }}">
+                                </div>
+                            </div>
                             <div class="col-12">
                                 <div class="input-group d-flex flex-column">
                                     <label for="send_address">Send Address</label>
