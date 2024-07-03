427a720b5492692c0c447bbb0faed6029db3ac94
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-03 17:47:18 +0800

    Progress: Update return transaction detail page

--- resources/views/dashboard/report-return-transaction/detail.blade.php
+++ resources/views/dashboard/report-return-transaction/detail.blade.php
@@ -14,13 +14,6 @@
                 <div class="card-default">
                     <form class="form d-flex flex-column gap-3 w-100">
                         <div class="row g-3">
-                            <div class="col-12">
-                                <div class="input-group d-flex flex-column">
-                                    <label for="customers_id">Customer</label>
-                                    <input type="text" class="input w-100" name="customers_id" id="customers_id" readonly
-                                        value="{{ $return->transaction->booking->customer->fullname }}">
-                                </div>
-                            </div>
                             <div class="col-md-6">
                                 <div class="input-group d-flex flex-column">
                                     <label for="pickup_date">Pickup Date</label>
@@ -37,6 +30,13 @@
                             </div>
                             <div class="col-md-6">
                                 <div class="input-group d-flex flex-column">
+                                    <label for="customers_id">Customer</label>
+                                    <input type="text" class="input w-100" name="customers_id" id="customers_id" readonly
+                                        value="{{ $return->transaction->booking->customer->fullname }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
                                     <label for="drivers_id">Driver</label>
                                     <input type="text" class="input w-100" name="drivers_id" id="drivers_id" readonly
                                         value="{{ $return->transaction->booking->driver != null ? $return->transaction->booking->driver->fullname : '-' }}">
@@ -47,6 +47,27 @@
                                     <label for="vehicles_id">Vehicle</label>
                                     <input type="text" class="input w-100" name="vehicles_id" id="vehicles_id" readonly
                                         value="{{ $return->transaction->booking->vehicle->name . ' - ' . $return->transaction->booking->vehicle->license_plate_number . ' - ' . $return->transaction->booking->vehicle->capacity . ' person' }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="total_price">Total Price</label>
+                                    <input type="text" class="input w-100" id="total_price" readonly
+                                        value="{{ formatRupiah($return->transaction->total_price) }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="total_paid">Total Paid</label>
+                                    <input type="text" class="input w-100" id="total_paid" readonly
+                                        value="{{ formatRupiah($return->transaction->total_paid) }}">
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="input-group d-flex flex-column">
+                                    <label for="total_change">Total Change</label>
+                                    <input type="text" class="input w-100" id="total_change" readonly
+                                        value="{{ formatRupiah($return->transaction->total_change) }}">
                                 </div>
                             </div>
                             <div class="col-12">
@@ -81,7 +102,8 @@
                             </div>
                             <div class="col-12">
                                 <div class="button-group d-flex gap-2">
-                                    <a href="{{ route('report-return-transaction.index') }}" class="button-reverse">Back to
+                                    <a href="{{ route('report-return-transaction.index') }}" class="button-reverse">Back
+                                        to
                                         Report Return
                                         Transaction Page</a>
                                 </div>
