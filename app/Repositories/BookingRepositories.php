c571517ae2f126627e57764d43d9244dd493ef26
ivanverdyansyah11 <ivanverdyansyah11@gmail.com>
2024-07-03 17:46:04 +0800

    Progress: Update booking repositories

--- app/Repositories/BookingRepositories.php
+++ app/Repositories/BookingRepositories.php
@@ -24,7 +24,7 @@ class BookingRepositories
 
             if (auth()->user()->admin) {
                 return $this->booking
-                    ->where('status', 1)
+                    ->whereNotIn('status', [0])
                     ->where(function ($query) use ($pickupDate, $returnDate) {
                         $query->whereBetween('pickup_date', [$pickupDate, $returnDate])
                             ->orWhereBetween('return_date', [$pickupDate, $returnDate])
@@ -33,10 +33,11 @@ class BookingRepositories
                                     ->where('return_date', '>=', $returnDate);
                             });
                     })
+                    ->latest()
                     ->paginate(6);
             } elseif (auth()->user()->driver) {
                 return $this->booking
-                    ->where('status', 1)
+                    ->whereNotIn('status', [0])
                     ->where('drivers_id', auth()->user()->driver->id)
                     ->where(function ($query) use ($pickupDate, $returnDate) {
                         $query->whereBetween('pickup_date', [$pickupDate, $returnDate])
@@ -46,10 +47,11 @@ class BookingRepositories
                                     ->where('return_date', '>=', $returnDate);
                             });
                     })
+                    ->latest()
                     ->paginate(6);
             } elseif (auth()->user()->customer) {
                 return $this->booking
-                    ->where('status', 1)
+                    ->whereNotIn('status', [0])
                     ->where('customers_id', auth()->user()->customer->id)
                     ->where(function ($query) use ($pickupDate, $returnDate) {
                         $query->whereBetween('pickup_date', [$pickupDate, $returnDate])
@@ -59,15 +61,16 @@ class BookingRepositories
                                     ->where('return_date', '>=', $returnDate);
                             });
                     })
+                    ->latest()
                     ->paginate(6);
             }
         } else {
             if (auth()->user()->admin) {
-                return $this->booking->where('status', 1)->latest()->paginate(6);
+                return $this->booking->whereNotIn('status', [0])->latest()->paginate(6);
             } elseif (auth()->user()->driver) {
-                return $this->booking->where('status', 1)->where('drivers_id', auth()->user()->driver->id)->latest()->paginate(6);
+                return $this->booking->whereNotIn('status', [0])->where('drivers_id', auth()->user()->driver->id)->latest()->paginate(6);
             } elseif (auth()->user()->customer) {
-                return $this->booking->where('status', 1)->where('customers_id', auth()->user()->customer->id)->latest()->paginate(6);
+                return $this->booking->whereNotIn('status', [0])->where('customers_id', auth()->user()->customer->id)->latest()->paginate(6);
             }
         }
     }
@@ -99,17 +102,17 @@ class BookingRepositories
         $pickupDate = Carbon::parse($pickup_date)->startOfDay();
         $returnDate = Carbon::parse($return_date)->endOfDay();
 
-        $bookings = $this->booking
-            ->where('status', 1)
-            ->where(function ($query) use ($pickupDate, $returnDate) {
-                $query->whereBetween('pickup_date', [$pickupDate, $returnDate])
-                    ->orWhereBetween('return_date', [$pickupDate, $returnDate])
-                    ->orWhere(function ($query) use ($pickupDate, $returnDate) {
-                        $query->where('pickup_date', '<=', $pickupDate)
-                            ->where('return_date', '>=', $returnDate);
-                    });
-            })
-            ->get(['drivers_id', 'vehicles_id']);
+        // $bookings = $this->booking
+        //     ->where('status', 1)
+        //     ->where(function ($query) use ($pickupDate, $returnDate) {
+        //         $query->whereBetween('pickup_date', [$pickupDate, $returnDate])
+        //             ->orWhereBetween('return_date', [$pickupDate, $returnDate])
+        //             ->orWhere(function ($query) use ($pickupDate, $returnDate) {
+        //                 $query->where('pickup_date', '<=', $pickupDate)
+        //                     ->where('return_date', '>=', $returnDate);
+        //             });
+        //     })
+        //     ->get(['drivers_id', 'vehicles_id']);
 
         $transactions = $this->transaction
             ->where('status', 1)
@@ -123,14 +126,14 @@ class BookingRepositories
             })
             ->get();
 
-        $driversIdsFromBookings = $bookings->pluck('drivers_id')->toArray();
-        $vehiclesIdsFromBookings = $bookings->pluck('vehicles_id')->toArray();
-        $driversIdsFromTransactions = $transactions->pluck('booking.drivers_id')->toArray();
-        $vehiclesIdsFromTransactions = $transactions->pluck('booking.vehicles_id')->toArray();
-        $driversIds = array_merge($driversIdsFromBookings, $driversIdsFromTransactions);
-        $vehiclesIds = array_merge($vehiclesIdsFromBookings, $vehiclesIdsFromTransactions);
-        $driversIds = array_unique($driversIds);
-        $vehiclesIds = array_unique($vehiclesIds);
+        // $driversIdsFromBookings = $bookings->pluck('drivers_id')->toArray();
+        // $vehiclesIdsFromBookings = $bookings->pluck('vehicles_id')->toArray();
+        $driversIds = $transactions->pluck('booking.drivers_id')->toArray();
+        $vehiclesIds = $transactions->pluck('booking.vehicles_id')->toArray();
+        // $driversIds = array_merge($driversIdsFromBookings, $driversIdsFromTransactions);
+        // $vehiclesIds = array_merge($vehiclesIdsFromBookings, $vehiclesIdsFromTransactions);
+        // $driversIds = array_unique($driversIds);
+        // $vehiclesIds = array_unique($vehiclesIds);
         return [$driversIds, $vehiclesIds];
     }
 
@@ -152,8 +155,27 @@ class BookingRepositories
     public function update($request, $id)
     {
         $booking = $this->findById($id);
+        $bookingSimilars = $this->booking->where('status', 1)->where('vehicles_id', $booking->vehicles_id)
+            ->whereNotIn('id', [$booking->id])
+            ->where(function ($query) use ($booking) {
+                $query->whereBetween('pickup_date', [$booking->pickup_date, $booking->return_date])
+                    ->orWhereBetween('return_date', [$booking->pickup_date, $booking->return_date])
+                    ->orWhere(function ($query) use ($booking) {
+                        $query->where('pickup_date', '<=', $booking->pickup_date)
+                            ->where('return_date', '>=', $booking->return_date);
+                    });
+            })->get();
+        $request['total_price'] = str_replace('Rp. ', '', $request['total_price']);
+        $request['total_price'] = (int) str_replace('.', '', $request['total_price']);
+        $request['total_paid'] = str_replace('Rp. ', '', $request['total_paid']);
+        $request['total_paid'] = (int) str_replace('.', '', $request['total_paid']);
+        $request['total_change'] = str_replace('Rp. ', '', $request['total_change']);
+        $request['total_change'] = (int) str_replace('.', '', $request['total_change']);
         $request['paid_image'] = $this->uploadFile->uploadSingleFile($request['paid_image'], "assets/image/paid");
         $booking->update(['status' => 0]);
+        foreach ($bookingSimilars as $bookingSimilar) {
+            $bookingSimilar->update(['status' => 2]);
+        }
         return $this->transaction->create($request);
     }
 
